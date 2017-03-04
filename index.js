76a9a560bb23c15542aacd6a35b855a88d611295
Jess Neary <Caneary@hotmail.com>
2017-03-04 14:13:36 +1300

    numbers from 1 to 15 calculating correctly

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 // converts inputed number to roman numeral
 module.exports = function convert (number) {
-  return 'X'.repeat(number)
+  let numeral = ''
+
+  const fives = Math.floor(number / 5)
+  const tens = Math.floor(number / 10)
+  const remainder = number % 5
+  // console.log('fives:',fives,'tens:', tens, 'remainder:', remainder)
+  if (tens) {
+    if (tens <= 3) {
+      numeral += 'X'.repeat(tens)
+    }
+    else if (tens === 4) {
+      numeral += 'XL'
+    }
+    else {
+      numeral += 'L'
+    }
+  }
+  if (fives % 2) {
+    if (remainder === 4) {
+      numeral += 'IX'
+    }
+    else {numeral += 'V'}
+  }
+  else if (remainder === 4) {
+    numeral += 'IV'
+  }
+  if (remainder <= 3) {
+    numeral += 'I'.repeat(remainder)
+  }
+  return numeral
 }
--- test.js
+++ test.js
@@ -2,13 +2,43 @@ var assert = require('assert')
 
 var convert = require('./index')
 
-describe('convert()', function () {
-  it('should return the correct value for numbers 1 through 3', function () {
+describe('convert() 1 to 5', function () {
+  it('should return the correct value for numbers 1 through 5', function () {
     var numbers = [1, 2, 3]
-    var expected = ['X', 'XX', 'XXX']
+    var expected = ['I', 'II', 'III']
     var actual = numbers.map(function (number) {
       return convert(number)
     })
-    assert.equal(actual, expected)
+    assert.deepEqual(actual, expected)
+  })
+  it('should return the correct value for 4', function () {
+    assert.deepEqual(convert(4), 'IV')
+  })
+  it('should return the correct value for 5', function () {
+    assert.deepEqual(convert(5), 'V')
+  })
+})
+describe('convert() 6 to 9', function () {
+  it('should return the correct value for 6 through 9', function () {
+    var numbers = [6, 7, 8, 9]
+    var expected = ['VI', 'VII', 'VIII', 'IX']
+    var actual = numbers.map(function (number) {
+      return convert(number)
+    })
+    assert.deepEqual(actual, expected)
+  })
+})
+describe('convert() 10 to 15', function () {
+  it('should return the correct values for 10 through 13', function () {
+    var numbers = [10, 11, 12, 13]
+    var expected = ['X', 'XI', 'XII', 'XIII']
+    var actual = numbers.map(function (number) {
+      return convert(number)
+    })
+    assert.deepEqual(actual, expected)
+  })
+  it('should return the correct values for 14 and 15', function () {
+    assert.equal(convert(14), 'XIV')
+    assert.equal(convert(15), 'XV')
   })
 })
