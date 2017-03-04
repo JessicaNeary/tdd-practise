4af0a3ea0b0915bd08a196e082f2b3934131100a
Jess Neary <Caneary@hotmail.com>
2017-03-04 14:50:37 +1300

    Tests passing for numbers up to (and including) 100

--- index.js
+++ index.js
@@ -3,18 +3,27 @@ module.exports = function convert (number) {
   let numeral = ''
 
   const fives = Math.floor(number / 5)
-  const tens = Math.floor(number / 10)
+  let tens = Math.floor(number / 10)
+  let hundreds = Math.floor(number / 100)
   const remainder = number % 5
   // console.log('fives:',fives,'tens:', tens, 'remainder:', remainder)
+  if (hundreds) {
+    numeral += 'C'
+    tens -= 10
+  }
   if (tens) {
+    if (tens === 9) {
+      numeral += 'XC'
+    }
+    else if (tens >= 5) {
+      numeral += 'L'
+      tens -= 5
+    }
     if (tens <= 3) {
       numeral += 'X'.repeat(tens)
     }
     else if (tens === 4) {
       numeral += 'XL'
-    }
-    else {
-      numeral += 'L'
     }
   }
   if (fives % 2) {
--- test.js
+++ test.js
@@ -42,8 +42,8 @@ describe('convert() numbers 10 to 19', function () {
     assert.equal(convert(19), 'XIX')
   })
 })
-describe.only('convert() numbers between 20 and 50', function () {
-  it('should return the correct values for values between 20 and 40', function () {
+describe('convert() numbers 20 to 50', function () {
+  it('should return the correct values for numbers 20 to 39', function () {
     var numbers = [20, 21, 29, 34, 36, 38]
     var expected = ['XX', 'XXI', 'XXIX', 'XXXIV', 'XXXVI', 'XXXVIII']
     var actual = numbers.map(function (number) {
@@ -51,9 +51,24 @@ describe.only('convert() numbers between 20 and 50', function () {
     })
     assert.deepEqual(actual, expected)
   })
-  it('should return the correct values for values between 40 and 50', function () {
+  it('should return the correct values for numbers 40 to 50', function () {
     assert.equal(convert(40), 'XL')
     assert.equal(convert(49), 'XLIX')
     assert.equal(convert(50), 'L')
+  })
+})
+describe('convert() numbers 50 to 100', function () {
+  it('should return the correct values for numbers 51 to 89', function () {
+    var numbers = [51, 63, 74, 79, 82, 89]
+    var expected = ['LI', 'LXIII', 'LXXIV', 'LXXIX', 'LXXXII', 'LXXXIX']
+    var actual = numbers.map(function (number) {
+      return convert(number)
+    })
+    assert.deepEqual(actual, expected)
+  })
+  it('should return the correct values for numbers 90 to 100', function () {
+    assert.equal(convert(90), 'XC')
+    assert.equal(convert(97), 'XCVII')
+    assert.equal(convert(100), 'C')
   })
 })
